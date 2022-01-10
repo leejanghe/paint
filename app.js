112409949ff4b7b2dad0845c1e635a1b32a974ba
leejanghe <babybye777@gmail.com>
2022-01-10 18:20:49 +0900

    canvas css완료 및 마우스 이벤트 설정

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('gggss')
+const canvas = document.getElementById('jsCanvas');
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+    // console.log(event)
+}
+
+let painting = false;
+
+function stopPaintng(event) {
+    painting = false;
+}
+
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+
+function onMouseUp(event){
+    stopPaintng();
+}
+
+function onMouseLeave(event) {
+    painting = false;
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+}
