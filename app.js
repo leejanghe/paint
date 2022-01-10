ba122aff035a5c39d17e5bedaef3305997b5f5d4
leejanghe <babybye777@gmail.com>
2022-01-11 02:57:37 +0900

    컨버스 색 채우기

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // 선채우기
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext("2d");
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 600;
 
-canvas.width = 600;
-canvas.height = 600;
-ctx.strokeStyle = 'black';
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 function onMouseMove(event) {
@@ -43,11 +45,20 @@ function onMouseLeave(event) {
     painting = false;
 }
 
+
+function handleCanvasClick(event){
+    if(filling){
+        ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+    
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", stratPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 
@@ -55,10 +66,7 @@ if(canvas){
 
 const colors = document.getElementsByClassName('jsColor');
 
-console.log(Array.from(colors))
-
-
-
+// console.log(Array.from(colors))
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 
@@ -67,6 +75,7 @@ function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     console.log(color)
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 
@@ -84,3 +93,26 @@ if(range){
     range.addEventListener("input", handleRangeChange);
 }
 
+// 켄버스 채우기
+
+const mode = document.getElementById('jsMode');
+
+
+
+let filling = false;
+ctx.fillStyle = INITIAL_COLOR;
+
+function handleModeClick(event){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill"
+    }else{
+        filling = true;
+        mode.innerText = "Paint"
+        ctx.fillStyle = ctx.strokeStyle;
+    }
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
